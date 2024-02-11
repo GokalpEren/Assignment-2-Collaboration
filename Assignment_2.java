44e9da124b6d784a085bf162c32a038e6a4fba94
Atahan Alp <atahanpla@gmail.com>
2024-02-11 12:02:12 +0300

    added Javadoc
    Edited the layout and added Javadoc to the necessary parts.

--- Assignment_2.java
+++ Assignment_2.java
@@ -1,125 +1,11 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.util.Arrays;
 
+/**
+ * Array functions operator and tester.
+ */
 public class Assignment_2 {
-    public static int findSum (int[] arr){
-        int sum = 0;
-        for (int num : arr) {
-            sum += num;
-        }
-        return sum;
-    }
-
-    public static double findAverage (int[] arr){
-        return (double)findSum(arr) / arr.length;
-    }
-
-    public static double[] diffFromAverage (int[] arr){
-        double[] diffArray = new double[arr.length];
-        double average = findAverage(arr);
-        for (int i = 0; i < arr.length; i++) {
-            diffArray[i] = arr[i] - average;
-        }
-        return diffArray;
-    }
-
-    public static int findMin(int[] arr){
-
-        int min = 101;
-
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] < min ){
-                min = arr[i];
-            }
-        }
-        return min ;
-    }
-
-    public static int findMax(int[] arr){
-
-        int max = -1;
-
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] > max ){
-                max = arr[i];
-            }
-        }
-        return max ;
-    }
-    public static void displayMenu(int[] arr) {
-        Scanner scanner = new Scanner(System.in);
-        int choice = 0;
-        while (choice != 5) {
-            System.out.println("\nMenu:");
-            System.out.println("1. Find minimum");
-            System.out.println("2. Find maximum");
-            System.out.println("3. Find average and differences from average");
-            System.out.println("4. Sum of elements with odd- and even-numbered indexes");
-            System.out.println("5. Exit");
-            System.out.print("Enter your choice: ");
-            choice = scanner.nextInt();
-
-            if (choice == 1) 
-            {
-                System.out.println("Minimum: " + findMin(arr));
-            } 
-            else if (choice == 2) 
-            {
-                System.out.println("Maximum: " + findMax(arr));
-            } 
-            else if (choice == 3) 
-            {
-                double average = findAverage(arr);
-                System.out.println("Average: " + average);
-                double[] differences = diffFromAverage(arr);
-            } 
-            else if (choice == 4) 
-            {
-                System.out.println("Sum of elements with odd indexes: " + sumOddIndexes(arr));
-                System.out.println("Sum of elements with even indexes: " + sumEvenIndexes(arr));
-            } 
-            else if (choice == 5) 
-            {
-                System.out.println("Exiting the program.");
-            } 
-            else 
-            {
-                System.out.println("Invalid choice! Please enter a valid option.");
-            }
-        }
-        scanner.close();
-
-        
-    }
-
-    public static int sumOddIndexes(int[] arr){
-
-        int sum = 0;
-        
-        for(int i = 0; i < arr.length; i++){
-            
-            if(arr[i] % 2 == 1){
-                sum += arr[i];
-            }
-        }
-
-        return sum;
-    }
-
-    public static int sumEvenIndexes(int[] arr){
-
-        int sum = 0;
-        
-        for(int i = 0; i < arr.length; i++){
-            
-            if(arr[i] % 2 == 0){
-                sum += arr[i];
-            }
-        }
-
-        return sum;
-    }
-
     public static void main(String[] args) {
         
         Scanner in = new Scanner (System.in);
@@ -131,11 +17,12 @@ public class Assignment_2 {
 
         //Checking for valid inputs for the size of the array
         do { 
-            if (!in.hasNextInt ()) {
+            if (!in.hasNextInt()) {
                 System.out.println ("Invalid input, please enter a positive integer.");
+                in.nextLine();
             } 
             else {
-                userInput = in.nextInt ();
+                userInput = in.nextInt();
 
                 if (userInput <= 0) {
                     System.out.println ("Invalid input, please enter a positive integer.");
@@ -158,8 +45,166 @@ public class Assignment_2 {
         for (int i = 0; i < randomizedArray.length; i++) {
             System.out.print (randomizedArray[i] + " ");
         }
+
         System.out.println ();
         displayMenu(randomizedArray);
+    }
 
+    /**
+     * Display and select the operations you can do with the the created array on the terminal.
+     * @param arr Integer array.
+     */
+    public static void displayMenu(int[] arr) {
+        Scanner in = new Scanner(System.in);
+        int choice = 0;
+
+        while (choice != 5) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Find minimum");
+            System.out.println("2. Find maximum");
+            System.out.println("3. Find average and differences from average");
+            System.out.println("4. Sum of elements with odd- and even-numbered indexes");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+
+            if (in.hasNextInt()) {
+                choice = in.nextInt();
+
+                if (choice == 1) {
+                    System.out.println("Minimum: " + findMin(arr));
+                } 
+                else if (choice == 2) {
+                    System.out.println("Maximum: " + findMax(arr));
+                } 
+                else if (choice == 3) {
+                    int average = findAverage(arr);
+                    System.out.println("Average (rounded to the closest integer): " + average);
+                    System.out.println("Differences of the elements from average: " + Arrays.toString(diffFromAverage(arr)));
+                } 
+                else if (choice == 4) {
+                    System.out.println("Sum of elements with odd indexes: " + sumOddIndexes(arr));
+                    System.out.println("Sum of elements with even indexes: " + sumEvenIndexes(arr));
+                } 
+                else if (choice == 5) {
+                    System.out.println("Exiting the program.");
+                } 
+                else {
+                    System.out.println("Invalid choice! Please enter a valid option.");
+                }
+            }
+            else {
+                System.out.println("Please enter an integer between 1-5.");
+                in.nextLine();
+            }
+        }
+        in.close();   
+    }
+
+    /**
+     * Find the minimum value of the array.
+     * @param arr Integer array.
+     * @return Minimum integer value of the given array.
+     */
+    public static int findMin(int[] arr){
+        int min = 101;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] < min ){
+                min = arr[i];
+            }
+        }
+        return min ;
+    }
+
+    /**
+     * Find the maximum value of the array.
+     * @param arr Integer array.
+     * @return Maximum integer value of the given array.
+     */
+    public static int findMax(int[] arr){
+        int max = -1;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > max ){
+                max = arr[i];
+            }
+        }
+        return max ;
+    }
+
+    /**
+     * Find the sum of all the elements of the array.
+     * @param arr Integer array.
+     * @return Sum of the elements.
+     */
+    public static int findSum (int[] arr){
+        int sum = 0;
+
+        for (int num : arr) {
+            sum += num;
+        }
+
+        return sum;
+    }
+
+    /**
+     * Find the average of the given array rounded to the closest integer value.
+     * @param arr Integer array.
+     * @return Average of the array rounded to the closest integer.
+     */
+    public static int findAverage (int[] arr){
+        return (int) Math.round((double)findSum(arr) / arr.length);
+    }
+
+    /**
+     * Substract the rounded average of the array from all the elements of the array.
+     * @param arr Integer array.
+     * @return Array of integers.
+     */
+    public static int[] diffFromAverage (int[] arr){
+        int[] diffArray = new int[arr.length];
+        int average = findAverage(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            diffArray[i] = arr[i] - average;
+        }
+
+        return diffArray;
+    }
+
+    /**
+     * Find the sum of the odd elements of the array.
+     * @param arr Integer array.
+     * @return Sum of the odd elements.
+     */
+    public static int sumOddIndexes(int[] arr){
+        int sum = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            
+            if(arr[i] % 2 == 1){
+                sum += arr[i];
+            }
+        }
+
+        return sum;
+    }
+
+    /**
+     * Find the sum of the even elements of the array.
+     * @param arr Integer array.
+     * @return Sum of the even elements.
+     */
+    public static int sumEvenIndexes(int[] arr){
+        int sum = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            
+            if(arr[i] % 2 == 0){
+                sum += arr[i];
+            }
+        }
+
+        return sum;
     }
 }
